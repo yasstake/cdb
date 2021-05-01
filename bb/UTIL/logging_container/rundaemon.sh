148d10ca7dfa19022b0a298656fb666458d95bc7
yass <yass@takibi.net>
2021-05-01 15:56:18 +0900

    Wait 2H before shutdown

--- bb/UTIL/logging_container/rundaemon.sh
+++ bb/UTIL/logging_container/rundaemon.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 docker rm -f BB-DAEMON-A 
-docker run --name BB-DAEMON-A -d -v /bitlog:/bitlog --restart=always -t bblogger /bin/logger -log_dir /bitlog/BB -flag_file  /bitlog/BBFLAG 120
+docker rm -f BB-DAEMON-B
+
+docker run --name BB-DAEMON-A -d -v /bitlog:/bitlog --restart=always -t bblogger /bin/logger -log_dir /bitlog/BB -flag_file  /bitlog/BBFLAG 120 -exit_wait 120
 
 sleep 120
 
-docker run --name BB-DAEMON-B -d -v /bitlog:/bitlog --restart=always -t bblogger /bin/logger -log_dir /bitlog/BB -flag_file  /bitlog/BBFLAG 120
+docker run --name BB-DAEMON-B -d -v /bitlog:/bitlog --restart=always -t bblogger /bin/logger -log_dir /bitlog/BB -flag_file  /bitlog/BBFLAG 120 -exit_wait 120
 
 
 
