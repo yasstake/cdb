d4b6120c85d7a6a4dbd6d8961808ab6ab179880f
yass <yass@takibi.net>
2021-05-16 20:14:20 +0900

    Change rundaemon

--- bb/UTIL/logging_container/rundaemon.sh
+++ bb/UTIL/logging_container/rundaemon.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 docker rm -f BB-DAEMON-A 
-docker rm -f BB-DAEMON-B
-
 docker run --name BB-DAEMON-A -d -v /bitlog:/bitlog --restart=always -t bblogger /bin/logger -log_dir /bitlog/BB -flag_file  /bitlog/BBFLAG -exit_wait 240
+sleep 240
 
-sleep 120
 
+docker rm -f BB-DAEMON-B
 docker run --name BB-DAEMON-B -d -v /bitlog:/bitlog --restart=always -t bblogger /bin/logger -log_dir /bitlog/BB -flag_file  /bitlog/BBFLAG -exit_wait 240
 
 
